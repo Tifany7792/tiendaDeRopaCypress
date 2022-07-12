91cb01f663ad3154d731d6a7fa11bb0e892b87f0
Tifany7792 <>
2022-07-12 21:00:27 +0200

    Solved T1 and T2

--- cypress/e2e/search.cy.js
+++ cypress/e2e/search.cy.js
@@ -1,13 +1,24 @@
 describe('Search elements',()=>{
+  beforeEach(()=>{
+    cy.visit('/');
+  })
   it('search for elements with multiple results', ()=>{
-      cy.visit('/');
-      cy.fixture('index').then((index)=>{
-        cy.get(index.searchBox).type('dress');
-        cy.get(index.searchButton).click();
-      })
-      cy.fixture('searchResult').then((searchResult)=>{
-        cy.get(searchResult.title).should('contain','dress');
+    cy.fixture('index').then((index)=>{
+      cy.get(index.searchBox).type('dress');
+      cy.get(index.searchButton).click();
+    })
+    cy.fixture('searchResult').then((searchResult)=>{
+      cy.get(searchResult.title).should('contain','dress');
       
-      })
+    })
+  })
+  it('search for elements with no results',()=>{
+    cy.fixture('index').then((index)=>{
+      cy.get(index.searchBox).type('querty');
+      cy.get(index.searchButton).click();
+    })
+    cy.fixture('searchResult').then((searchResult)=>{
+      cy.get(searchResult.alert).should('contain','No results were found for your search');
+    })
   })
 })
